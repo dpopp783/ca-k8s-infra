57e69c169942d076d852a57a1f9682a4dbb0b03b
Daniel Popp <dpopp783@gmail.com>
2025-03-20 06:35:57 +0000

    update minio keys

--- training/train.py
+++ training/train.py
@@ -76,8 +76,8 @@ def train_func(config):
 
     # Initialize the S3 filesystem
     fs = S3FileSystem(
-        access_key="minioadmin",
-        secret_key="minioadmin",  # noqa
+        access_key="7crUX7^HA#hIUUJs",
+        secret_key="6RU*Ji4B7mP0T*iZ",  # noqa
         endpoint_override=f"minio.minio-internal-{os.environ['ENVIRONMENT']}.svc.cluster.local:9000",
         scheme="http",
     )
